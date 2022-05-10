c9404e109170e1a11966a04cd260dbddaafc7dc4
Mohamad J. Makki <malanbaki@transforat.com>
2022-05-10 09:12:41 +0300

    Golang Gin-Gonic Multi-Authentication System

--- api/auth.go
+++ api/auth.go
@@ -5,6 +5,20 @@ import (
 	"net/http"
 )
 
+type RegisterInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
 func Register(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"data": "hello from controller!"})
+
+	var input RegisterInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "validated!"})
+
 }
