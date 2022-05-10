035f8ed5794baad13c3939bfd7c517cd499d4483
Mohamad J. Makki <malanbaki@transforat.com>
2022-05-10 09:15:26 +0300

    Golang Gin-Gonic Multi-Authentication System

---
+++ entity/user.go
@@ -0,0 +1,11 @@
+package entity
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username string `gorm:"size:255;not null;unique" json:"username"`
+	Password string `gorm:"size:255;not null;" json:"password"`
+}
