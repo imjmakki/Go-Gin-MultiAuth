b2135409381dbd77cc1c8db2a463d1db629f35d8
Mohamad J. Makki <malanbaki@transforat.com>
2022-05-10 09:30:13 +0300

    Golang Gin-Gonic Multi-Authentication System

---
+++ middlewares/middlewares.go
@@ -0,0 +1,20 @@
+package middlewares
+
+import (
+	"Multi-Auth/utils/token"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func JwtAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := token.TokenValid(c)
+		if err != nil {
+			c.String(http.StatusUnauthorized, "Unauthorized")
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
